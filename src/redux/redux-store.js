3726ecd7ba9ae8fba07fc7df71d39dfe9118ea9e
black1ight <lasunaleksandr@gmail.com>
2023-02-16 22:49:36 +0200

    add Status

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,12 +1,19 @@
-import {combineReducers, legacy_createStore as createStore} from "redux"
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux"
 import profileReducer from "./profile-reducer"
 import dialogsReducer from "./dialogs-reducer"
+import usersReducer from "./users-reducer"
+import authReducer from "./auth-reducer"
+import ThunkMiddleware from "redux-thunk"
 
 let reducers = combineReducers({
     profilePage: profileReducer,
-    dialogsPage: dialogsReducer
+    dialogsPage: dialogsReducer,
+    usersPage: usersReducer,
+    auth: authReducer
 })
 
-let store = createStore(reducers)
+let store = createStore(reducers, applyMiddleware(ThunkMiddleware))
+
+window.store = store
 
 export default store
